807a8faff31904fa23d3b6de3385771959f95b89
roy-cruz <roy.cruz@upr.edu>
2024-01-23 20:19:04 +0100

    Added rrr utility functions

--- utils/refrank_utils.py
+++ utils/refrank_utils.py
@@ -1,5 +1,7 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 import numpy as np
+import seaborn as sns
 from sklearn.preprocessing import StandardScaler
 from sklearn.decomposition import PCA
 
@@ -21,7 +23,7 @@ def get_weights(df, standardize=True, plot=False):
     if plot:
         sns.pairplot(df)
         plt.show()
-    pca = PCA()
+    pca = PCA(n_components=1)
     pca.fit(df)
     pc1_loadings = pca.components_[0]
     pc1_df = pd.DataFrame(pc1_loadings, index=df.columns, columns=['PC1 Loadings'])
@@ -30,7 +32,7 @@ def get_weights(df, standardize=True, plot=False):
     return pc1_df['weights'].to_numpy()
 
 
-def refrank_pca(features, target, standardize=True, n_components=None, keep_original=False):
+def refrank_pca(features, target, standardize=True, n_components=None):#, keep_original=False):
     """
     Takes in a dataframe of features and ranks each point in feature space by their closseness to the target point. Distance measured in the n-th dimensional PCA sub-space using Eucledian distance. Note that only runs before the target run are considered for ranking.
     
@@ -52,11 +54,16 @@ def refrank_pca(features, target, standardize=True, n_components=None, keep_orig
         print("ERROR: Not enough runs to perform ranking")
         return None
     
+#     if keep_original:
+#         features_original = features.copy(deep=True)
+        
     run_nums = features.reset_index()['run']
     
     if standardize:
         scaler = StandardScaler()
         features = pd.DataFrame(scaler.fit_transform(features), columns=features.columns)
+        
+    # If n_components not, specified, set it to max.
     if n_components is None:
         n_components = len(features.columns)
         
@@ -74,4 +81,63 @@ def refrank_pca(features, target, standardize=True, n_components=None, keep_orig
     features_PC.rename(columns = {0:'dist'}, inplace=True)
     features_PC = features_PC.sort_values(by='dist', ascending=True).reset_index()
     
+#     if keep_original: 
+#         features_PC = pd.merge(features_PC, features_original, left_index=True, right_index=True, how='left')
+    
     return features_PC
+
+def comp_temp_dist(run_df, target):
+    """
+    Computer the temporal distance between a given target run and each of the 
+    candidate runs and adds these results as a column to the input run dataframe.
+    
+    Args:
+        run_df: Dataframe containing each run (target and candidates) with at least its start time.
+        target: Target run which will serve as temporal reference.
+        
+    Returns:
+        run_df, but with added temp_dist column
+    """
+    
+    # Convert data types
+    run_df['start_time'] = pd.to_datetime(run_df['start_time'])
+    run_df['end_time'] = pd.to_datetime(run_df['end_time'])
+    
+    try: # Try to get target time
+        target_time = run_df[run_df['run_number']==target]['start_time'].item()
+    except:
+        print("ERROR: Target not found in given dataframe.")
+        return run_df
+    
+    # Getting temporal distance
+    run_df['temp_dist'] = (target_time - run_df['start_time']).dt.total_seconds()
+    return run_df
+
+
+def comp_delta_totallumi(run_df):
+    """
+    Compute the change in total luminosity for each run using run level data.
+    
+    Args:
+        run_df: Dataframe containing each run (target and candidates) with at least its initial and final luminosity.
+        
+    Returns:
+        run_df, but with added delta_totallumi column
+    """
+    run_df['delta_totallumi'] = run_df['end_lumi'] - run_df['init_lumi']
+    
+    return run_df
+
+def comp_duration(run_df):
+    """
+    Compute the duration of each run in seconds.
+    
+    Args:
+        run_df: Dataframe containing each run (target and candidates) with at least its start and end time.
+        
+    Returns:
+        run_df, but with added duration column
+    """
+    
+    run_df['duration'] = (run_df['end_time'] - run_df['start_time']).dt.total_seconds()
+    return run_df
