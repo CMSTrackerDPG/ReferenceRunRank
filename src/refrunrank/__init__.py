517680a4106e015c2cf5b40fd8512a1713a6fb39
roy-cruz <roy.cruz@upr.edu>
2024-03-18 18:03:03 +0100

    CHRunData class development with general filter capabilities

--- src/refrunrank/__init__.py
+++ src/refrunrank/__init__.py
@@ -2,9 +2,9 @@
 
 import json
 import pandas as pd
-# CertHelperAPIJSONFilename="AllRunsRefRuns_Mar5_2024.json"
-# RefRunsJSONFile=open(CertHelperAPIJSONFilename, 'r')
-# RefRunsJSON=json.load(RefRunsJSONFile)
+from fnmatch import fnmatch
+
+
 class CHRunData:
     """Creating a simple class to organize the Reference Runs information from the CertHelper API"""
 
@@ -15,54 +15,39 @@ class CHRunData:
         self.JSON = json.load(self.JSONFile)
         self.RunsDF = pd.DataFrame(self.JSON)
         self.RunsDF.dropna(inplace=True)
+        self.filteredDF = None
         self.AllRunsByYear = {}
 
-    def getListOfRuns(self, type="all"):
-        """Processing the data in the "JSON" to produce the interesting lists of runs"""
-        if type == "all":
-            self.AllRuns = []
-            for entry in self.JSON:
-                runNumber = entry["run_number"]
-                self.AllRuns.append(
-                    runNumber
-                )  # Add all entries in this list, do more (cleaning/filtering) operations later
-                # if runNumber not in self.AllRuns:
-                #    self.AllRuns.append(runNumber)
-            return self.AllRuns
-        if type in ["2016", "2017", "2018", "2022", "2023", "2024"]:
-            self.AllRunsByYear[type] = []
-            for entry in self.JSON:
-                # print(type)
-                # print(entry['run_number'])
-                # print(entry['dataset'])
-                # Found that we have some dataset set to None, breaking the code, so catching the issue:
-                if isinstance(entry["dataset"], str):
-                    if type in entry["dataset"]:
-                        runNumber = entry["run_number"]
-                        self.AllRunsByYear[type].append(
-                            runNumber
-                        )  # Add all entries in this list, do more (cleaning/filtering) operations later
-                else:
-                    print(
-                        "The following entry does not have a dataset string in the input JSON!"
-                    )
-                    print(entry)
-            return self.AllRunsByYear[type]
+    def applyfilter(self, filters={}):
+        if len(filters) == 0:
+            print("Warning: No filter conditions given.")
+            return self.RunsDF
 
-    def getInfoForRun(self, runNumber):
-        """Quick function to dump information in the JSON about a given run"""
-        self.getListOfRuns()
-        if int(runNumber) not in self.AllRuns:
-            print(
-                "Sorry run %s was not found in the currently loaded JSON file %s"
-                % (runNumber, self.JSONFilename)
-            )
-            # return self.AllRuns
-            return
-        else:
-            entries = []
-            for entry in self.JSON:
-                if entry["run_number"] == int(runNumber):
-                    # print(entry)
-                    entries.append(entry)
-            return entries
+        mask = pd.Series([True] * len(self.RunsDF), index=self.RunsDF.index)
+
+        for key, value in filters.items():
+            if isinstance(value, tuple) and key in [
+                "run_number",
+                "reference_run_number",
+            ]:
+                mask &= (self.RunsDF[key] >= value[0]) & (self.RunsDF[key] <= value[1])
+            elif isinstance(value, list) and key in [
+                "run_number",
+                "reference_run_number",
+            ]:
+                mask &= self.RunsDF[key].isin(value)
+            elif isinstance(value, (int, float)) and key in [
+                "run_number",
+                "reference_run_number",
+            ]:
+                mask &= self.RunsDF[key] == value
+            elif isinstance(value, str) and key in [
+                "run_reconstruction_type",
+                "reference_run_reconstruction_type",
+                "dataset",
+            ]:
+                mask &= self.RunsDF[key].apply(lambda x: fnmatch(x, value))
+
+        # filteredDF = self.RunsDF[mask]
+        self.filteredDF = self.RunsDF[mask]
+        return self.filteredDF
